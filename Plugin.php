a477a2ee97a120c1cf202f69a838c2f9f444d177
浪子 <jkjoy@live.cn>
2024-03-19 12:05:28 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,28 +1,48 @@
 <?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 /**
- * 博客评论通过QQ机器人发送到QQ 使用默认API需添加QQ 2280858259 为好友 才能正常接收通知
+ * 博客评论通过QQ机器人通知QQ 使用默认API需添加QQ 2280858259 为好友 才能正常接收通知
  *
  * @package CommentsByQQ
  * @author 老孙
  * @version 1.0.4
  * @link https://blog.asbid.cn/
  */
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-
 class CommentsByQQ_Plugin implements Typecho_Plugin_Interface
 {
-
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常.
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
     public static function activate()
     {
         Typecho_Plugin::factory('Widget_Feedback')->finishComment = array('CommentsByQQ_Plugin', 'render');
         Typecho_Plugin::factory('Widget_Comments_Edit')->finishComment = array('CommentsByQQ_Plugin', 'render');
     }
 
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常.
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
     public static function deactivate()
     {
       
     }
 
+    /**
+     * 获取插件配置面板.
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
     public static function config(Typecho_Widget_Helper_Form $form)
     {
         $default_url = 'https://bot.asbid.cn';
@@ -38,11 +58,32 @@ class CommentsByQQ_Plugin implements Typecho_Plugin_Interface
         $form->addInput($qqboturl);
     }
 
+    /**
+     * 个人用户的配置面板.
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
     public static function personalConfig(Typecho_Widget_Helper_Form $form)
     {
 
     }
 
+    /**
+     * 发送QQ消息.
+  
+       你有新的评论：「{评论作者}」在文章《{文章标题}》中发表了评论！
+       评论内容：{评论正文}
+       永久链接地址:
+  
+       {永久链接地址}
+  
+     *
+     * @access public
+     * @param $comment 调用参数
+     * @return void
+     */
     public static function render($comment)
     {
         $options = Helper::options();
@@ -59,12 +100,13 @@ class CommentsByQQ_Plugin implements Typecho_Plugin_Interface
 			return;
 		}
       
-        $msg = '你有新的评论：\n「' . $comment->author . '」在文章《' . $comment->title . '》中发表了评论！';
-        $msg .= "\n评论内容:\n {$comment->text}\n永久链接地址：{$comment->permalink}";
+        $msg = '「' . $comment->author . '」在文章《' . $comment->title . '》中发表了评论！';
+ 
+        $msg .= "\n评论内容:\n{$comment->text}\n永久链接地址：{$comment->permalink}";
 
         $_message_data_ = array(
             'user_id' => (int) trim($options->plugin('CommentsByQQ')->qq),
-            'message' => str_replace(array("\r\n", "\r", "\n"), "\r\n",htmlspecialchars_decode(strip_tags($msg))) 
+            'message' => str_replace(array("\r\n", "\r", "\n"), "\r\n",htmlspecialchars_decode(strip_tags($msg))) // Tomloi 2021-6-23：使用指定方式更改消息格式 
          );
          
          $ch = curl_init();
